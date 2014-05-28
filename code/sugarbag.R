bfe411e16a951c26196d3515bfadd633423bb51d
David LeBauer <dlebauer@illinois.edu>
2014-05-27 19:45:36 -0500

    updated code for importing and joining tables

--- code/sugarbag.R
+++ code/sugarbag.R
@@ -1,4 +1,5 @@
 setwd(system.file("extdata", package = "sugarbag"))
+library(udunits2)
 
 ## Reading Fixed Information
 Fx.ClimateVar <- read.csv("Climate.Variables.csv")
@@ -14,34 +15,75 @@ Fx.Soil <- read.csv("Soil.csv")
 Fx.Soil.Layers <- read.csv("Soil.Layers.csv")
 Fx.Soil.Variables <- read.csv("Soil.Variables.csv")
 Fx.Weather.Station <- read.csv("Weather.Station.csv")
-
-
-Experiment.Summary <- read.csv("Experiment.Summary.csv")
-
-## traits and yields
-Harvest.Data <- read.csv("Harvest.Data.csv")
-yields <- with(Harvest.Data,
-               data.frame(ExpID,
-                          Plot,
-                          Date = dmy(Date),
-                          mean = TDWT))
-yields <- yields[!is.na(yields$mean),]
-ggplot(yields, aes(Date, mean, group = ExpID, color = Plot)) + geom_point()
-
-traits <- Harvest.Data[,-which(colnames(Harvest.Data) %in% c("X", "TDWT"))]
-traits$Date <- dmy(traits$Date)
-traits <- melt(traits, id.vars = 1:4, na.rm = TRUE)
-
-List.Of.Experiment <- read.csv("List.Of.Experiments.csv")
+# List.Of.Experiment <- read.csv("List.Of.Experiments.csv") only contains expt name
 Notes <- read.csv("Notes.csv")
-
+Experiment.Summary <- read.csv("Experiment.Summary.csv")
 ResearcherPerExperiment <- read.csv("ResearchersPerExperiment.csv")
 Weather.Data <- read.csv("Weather.Data.csv")
 
+## traits and yields
+Harvest.Data <- read.csv("Harvest.Data.csv")
 
-## treatments
-Experiment.Design <- read.csv("Experiment.Design.csv")
-Experiment.Design2 <- Experiment.Design[,-which(colnames(Experiment.Design) %in% c("X"))]
+
+traits <- data.table(Harvest.Data, key = c("ExpID", "Plot", "Date", "Sample"))
+traits <- data.table(melt(traits, id.var = c("ExpID", "Plot", "Date", "Sample"), variable.name = "variables.name", value.name = "mean", na.rm = TRUE))
+traits$Date <- lubridate::dmy(traits$Date)
+
+
+## cultivars
+Experiment.Design <- data.table(read.csv("Experiment.Design.csv"), key = 'ExpID,Plot')
+cultivars <- Experiment.Design[!is.na(Cultivar),list(ExpID, Plot, Cultivar)]
+
+traits <- merge(traits, cultivars, by = c("ExpID", "Plot"))
+
+## sites
+
+sites <- data.table(Fx.Experiment.Site, key = "SiteID")
+experiment.summary <- data.table(Experiment.Summary, key = "ExpID")
+experiments_sites <- data.table(experiment.summary)[,list(ExpID, MetStation, planting = BeginDate), by = "SiteID"]
+experiments_sites$planting <- mdy(experiments_sites$planting)
+sites <- merge(sites, experiments_sites)
+
+bety.sites <- sites[,list(sitename = SiteName, lat = Latitude, lon = Longitude, city = City, state = Region), by = "SiteID"]
+
+traits <- merge(traits, sites, by = "ExpID")
+
+## Variables
+variables <- data.table(read.csv("all.variables.csv"))
+if(sum(!unique(traits$variables.name) %in% variables$VariableName)>0){
+  stop(paste("missing", unique(traits$variables.name)[which(!unique(traits$variables.name) %in% variables$VariableName)]))
+  
+}
+
+variables <- variables[, list(variables.name = VariableName, 
+                              variables.description = VariableDescription,
+                              units = Units)]
+traits <- merge(traits, variables, by = 'variables.name')
+
+
+traits[units == "g/m2"]$mean <- traits[units == "g/m2", ud.convert(mean, "g/m2", "Mg/ha")]
+traits[units == "g/m2"]$units <- "Mg/ha"
+
+setnames(traits, c("Plot", "Date", "Cultivar", "SiteName", "City", "Region", "Latitude", "Longitude", "Elevation"), c("entity_id", "date", "cultivars.name", "sites.sitename", "sites.city", "sites.state", "lat", "lon", "masl"))
+
+### Yields: total aboveground dry weight
+
+yields <- traits[variables.name == "DWTOTA",]
+hist(traits[variables.name == "DWTOTA", mean])
+
+
+experiment.design <- data.table(melt(Experiment.Design, id.var = c('ExpID','Treatment','Rep', 'Plot'), variable.name = "managements.type", value.var = "managements.level"), na.rm = TRUE)
+
+## assign '1' to missing values of Replication  
+experiment.design[is.na(Rep)]$Rep <- 1
+
+traits <- merge(traits, experiment.design, by = c('ExpID','Plot'))
+
+
+yields <- traits[variables.name == "TDWT",]
+
+
+Experiment.Design <- Experiment.Design[,-which(colnames(Experiment.Design) %in% c("X"))]
 
 cultivars <- Experiment.Design2[, c("ExpID", "Treatment", "Rep", "Plot", "Cultivar")]
 treatments <- cbind(Experiment.Design2[,1:4], 
@@ -97,13 +139,10 @@ sites <- with(Fx.Experiment.Site,
 
 ## merge yields w/ meta-data
 
-
-Y <- merge(yields, cultivars, by = c("ExpID", "Plot"))
-Y2 <- merge(Y, treatments, by = c("ExpID", "Plot", "Treatment", "Rep"))
-Y3 <- merge(Y2, fertilizerN.total, by = c("ExpID", "Treatment"))
+Y <- merge(yields, cultivars, by = c("ExpID", "Plot"), all = TRUE)
+Y2 <- merge(Y, treatments, by = c("ExpID", "Plot", "Treatment", "Rep"), all = TRUE)
+Y3 <- merge(Y2, fertilizerN.total, by = c("ExpID", "Treatment"), all = TRUE)
 ggplot(data = Y3, aes(fertilizerN, mean)) + geom_point()
-
-
 
 ## transform Harvest Data from wide to long
 
@@ -113,55 +152,14 @@ ggplot(data = Y3, aes(fertilizerN, mean)) + geom_point()
 
 ## Reading Data Table for above ground biomass
 ABGDB <- Harvest.Data[!(is.na(Harvest.Data$DWTOTA)),c(1,3,4,5,35)]
-Nrate <- numeric(dim(ABGDB)[1])
-Nrate <- NA
-ABGDB <- cbind(ABGDB,Nrate)
+
+experiment.design <- Experiment.Design2
 experiment.design <- as.numeric(levels(experiment.design)[experiment.design])
-experiment.design <- as.numeric(levels(experiment.design)[experiment.design])
+experiment.design <- data.table(as.numeric(levels(experiment.design)[experiment.design]))
 
 for ( i in 1: length(Nrate))
   {
     expID <- ABGDB[i,]$ExpID
     pplot <- ABGDB[i,]$Plot
-    ABGDB[i,]$Nrate <- experiment.design[((experiment.design$ExpID==expID)&&(experiment.design$Plot==pplot)),]$NRate
+    ABGDB[i,]$Nrate <- experiment.design[((ExpID==expID)&&(experiment.design$Plot==pplot)),]$NRate
   }
-# Table citations
-
-id <- 99999
-author <- "Prestwidge and Laredo"
-year <- 2003
-title <- "SuGARBAG: A Database System for Sugarcane Crop Growth , Climate, Soils and Management Data"
-journal <- "CRC Sugar Occasional Publication Brisbane"
-vol="NA"
-pg="NA"
-url="NA"
-pdf="NA"
-created_at <- 8888
-updated_at <- 8888
-doi="NA"
-citations <- data.frame(id=id,author=author,year=year,title=title,journal=journal,vol=vol,pg=pg,url=url,pdf=pdf,created_at=created_at,updated_at=updated_at,doi=doi)
-
-
-## Table for citations_sites
-sites_id  <- Fx.Experiment.Site$Site.ID
-citation_id <- rep(id,length(sites_id))
-citations_sites <- data.frame(citation_id=citation_id,sites_id=sites_id,created_at=created_at,updated_at=updated_at)
-
-#Table for citation_treatments
-RNCEP_extraction <- data.frame(year=numeric(0),month=numeric(0), date=numeric(0), hr=numeric(0), Temp=numeric(0))
-folla <- rep(0,25)
-for (mm in 1:12)
-{
-for (dd in 1:30){
-  folla <- NCEP.interp(variable='air',level=850,lat=45,lon=100,dt=paste("2006-",mm,"-",dd," 00:00:00",sep=""))
-  write.table(folla, file="/home/djaiswal/rncep.trial.txt", append=TRUE)
-   folla <- NCEP.interp(variable='air',level=850,lat=45,lon=100,dt=paste("2006-",mm,"-",dd," 06:00:00",sep=""))
-   write.table(folla, file="/home/djaiswal/rncep.trial.txt", append=TRUE)
-   folla <- NCEP.interp(variable='air',level=850,lat=45,lon=100,dt=paste("2006-",mm,"-",dd," 12:00:00",sep=""))
-   write.table(folla, file="/home/djaiswal/rncep.trial.txt", append=TRUE)
-   folla <- NCEP.interp(variable='air',level=850,lat=45,lon=100,dt=paste("2006-",mm,"-",dd,"18:00:00",sep=""))
-   write.table(folla, file="/home/djaiswal/rncep.trial.txt", append=TRUE)
- }
-}
-
-  
