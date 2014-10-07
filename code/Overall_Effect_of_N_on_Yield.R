c62778a3cf88c5d938840e893b10cd27dee2193c
Deepak Jaiswal <deepakuiuc@gmail.com>
2014-10-06 20:53:43 -0500

    Comments are added in Overall_Effect_of_N_on_Yield.R

--- code/Overall_Effect_of_N_on_Yield.R
+++ code/Overall_Effect_of_N_on_Yield.R
@@ -1,25 +1,23 @@
-# Step 1:- Get all the experiment IDs
+# Step 1:- Get all the experiment IDs  for whic Harvest, Experiment Design and Planting information is available
 expID <-GetExpID()
 
 # Step 2 :- Define variable name that we are interested in
 varname <- "DWMST"
 
+# Creating initial dataframe that will be used for RBIND
 id <- expID[1]
 Output <- Total_N_for_expID(id, varname)
 N = length(expID)
 
+# Get N versus Yield data from all the experiments andRBIND them to Output
 for (i in 2:N){
   id <- expID[i]
   tmp_output <- Total_N_for_expID(id, varname)
   Output <- rbind(Output,tmp_output)
 }
 
-for (i in 76:N){
-  id <- expID[i]
-  tmp_output <- Total_N_for_expID(id, varname)
-  Output <- rbind(Output,tmp_output)
-}
 
+# Plot Yield versus N for all the Experiments
 library(lattice)
 png(file = "/home/djaiswal/Desktop/N_summary.png")
 Output$ExpID = as.factor(Output$ExpID)
