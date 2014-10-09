0ef13407e44cf4a720ecd4b72ce2bb216da57343
David LeBauer <dlebauer@illinois.edu>
2014-10-09 14:15:15 -0500

    modified creatingRdata.R script to be more easily ported across machines

--- code/creatingRdata.R
+++ code/creatingRdata.R
@@ -1,25 +1,27 @@
 ' The following code is simply generating .rda file for
   the dependent variables (measured) in SUGARBAG
 '
+setwd("/home/djaiswal/Research/gitprojects/sugarbag")
+#setwd("/Users/David LeBauer/R-dev/sugarbag")
 # Saving Experiment Lists
-ExperimentList<-read.csv("/home/djaiswal/Research/gitprojects/sugarbag/inst/extdata/List.Of.Experiments.csv", stringsAsFactors=FALSE)
+ExperimentList<-read.csv("inst/extdata/List.Of.Experiments.csv", stringsAsFactors=FALSE)
 ExperimentList<-ExperimentList[,c(1,2)]
-save(ExperimentList, file="/home/djaiswal/Research/gitprojects/sugarbag/data/ExperimentList.rda")
+save(ExperimentList, file="data/ExperimentList.rda")
 
 # Saving Harvest Data
-HarvestData<-read.csv("/home/djaiswal/Research/gitprojects/sugarbag/inst/extdata/Harvest.Data.csv", stringsAsFactors=FALSE)
-save(HarvestData, file="/home/djaiswal/Research/gitprojects/sugarbag/data/HarvestData.rda")
+HarvestData<-read.csv("inst/extdata/Harvest.Data.csv", stringsAsFactors=FALSE)
+save(HarvestData, file="data/HarvestData.rda")
 
 
 #Saving Weather Data
-WeatherData<-read.csv("/home/djaiswal/Research/gitprojects/sugarbag/inst/extdata/Weather.Data.csv", stringsAsFactors=FALSE)
+WeatherData<-read.csv("inst/extdata/Weather.Data.csv", stringsAsFactors=FALSE)
 WeatherData<-WeatherData[,c(1,3,4,5,6,7,8)]
-save(WeatherData, file="/home/djaiswal/Research/gitprojects/sugarbag/data/WeatherData.rda")
+save(WeatherData, file="data/WeatherData.rda")
 
 
 #Saving Soil Layer Data
-SoilLayersData<-read.csv("/home/djaiswal/Research/gitprojects/sugarbag/inst/extdata/Soil.Layer.Data.csv", stringsAsFactors=FALSE)
-save(SoilLayersData, file="/home/djaiswal/Research/gitprojects/sugarbag/data/SoilLayersData.rda")
+SoilLayersData<-read.csv("inst/extdata/Soil.Layer.Data.csv", stringsAsFactors=FALSE)
+save(SoilLayersData, file="data/SoilLayersData.rda")
 
 
 
@@ -29,27 +31,27 @@ available in the Experiment File
 '
 
 # Saving Experiment Summary
-ExperimentSummary<-read.csv("/home/djaiswal/Research/gitprojects/sugarbag/inst/extdata/Experiment.Summary.csv", stringsAsFactors=FALSE)
-save(ExperimentSummary, file="/home/djaiswal/Research/gitprojects/sugarbag/data/ExperimentSummary.rda")
+ExperimentSummary<-read.csv("inst/extdata/Experiment.Summary.csv", stringsAsFactors=FALSE)
+save(ExperimentSummary, file="data/ExperimentSummary.rda")
 
 # Saving Experiment Design
-ExperimentDesign<-read.csv("/home/djaiswal/Research/gitprojects/sugarbag/inst/extdata/Experiment.Design.csv", stringsAsFactors=FALSE)
-save(ExperimentDesign, file="/home/djaiswal/Research/gitprojects/sugarbag/data/ExperimentDesign.rda")
+ExperimentDesign<-read.csv("inst/extdata/Experiment.Design.csv", stringsAsFactors=FALSE)
+save(ExperimentDesign, file="data/ExperimentDesign.rda")
 
 #Saving Experiment Field
-FixedFields<-read.csv("/home/djaiswal/Research/gitprojects/sugarbag/inst/extdata/Experiment.Field.csv", stringsAsFactors=FALSE)
+FixedFields<-read.csv("inst/extdata/Experiment.Field.csv", stringsAsFactors=FALSE)
 FixedFields<-FixedFields[,c(1,3:9,11)]
-save(FixedFields, file="/home/djaiswal/Research/gitprojects/sugarbag/data/FixedFields.rda")
+save(FixedFields, file="data/FixedFields.rda")
 
 # Saving Fertilisation
-Fertilisation<-read.csv("/home/djaiswal/Research/gitprojects/sugarbag/inst/extdata/Fertilization.csv", stringsAsFactors=FALSE)
-save(Fertilisation, file="/home/djaiswal/Research/gitprojects/sugarbag/data/Fertilisation.rda")
+Fertilisation<-read.csv("inst/extdata/Fertilization.csv", stringsAsFactors=FALSE)
+save(Fertilisation, file="data/Fertilisation.rda")
 
 # Saving Planting Information
-Planting<-read.csv("/home/djaiswal/Research/gitprojects/sugarbag/inst/extdata/Planting.csv", stringsAsFactors=FALSE)
-save(Planting, file="/home/djaiswal/Research/gitprojects/sugarbag/data/Planting.rda")
+Planting<-read.csv("inst/extdata/Planting.csv", stringsAsFactors=FALSE)
+save(Planting, file="data/Planting.rda")
 
 
 # Saving Experiment Sites
-Experiment.Site<-read.csv("/home/djaiswal/Research/gitprojects/sugarbag/inst/extdata/Experiment.Site.csv", stringsAsFactors=FALSE)
-save(Experiment.Site, file="/home/djaiswal/Research/gitprojects/sugarbag/data/Experiment.Site.rda")
+Experiment.Site<-read.csv("inst/extdata/Experiment.Site.csv", stringsAsFactors=FALSE)
+save(Experiment.Site, file="data/Experiment.Site.rda")
