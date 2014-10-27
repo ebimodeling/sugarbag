0912fccaaf8ff23c9d94b79597590e044d9b131f
David LeBauer <dlebauer@illinois.edu>
2014-10-27 12:51:55 -0500

    updated sugarbag script

--- code/sugarbag.R
+++ code/sugarbag.R
@@ -1,13 +1,48 @@
 library(sugarbag)
+library(udunits2)
+library(dplyr)
+#setwd(system.file("extdata", package = "sugarbag"))
+setwd("/Users/David LeBauer/R-dev/sugarbag/inst/extdata/")
+## Reading Fixed Information
+Fx.ClimateVar <- fread("Climate.Variables.csv")
+Fx.Crop.Variables <- fread("Crop.Variables.csv")
+Fx.Experiment.Field <- fread("Experiment.Field.csv")
+Fx.Experiment.Site <- fread("Experiment.Site.csv")
+Fx.Factors <- fread("Factors.csv")
+Fx.Fertilizer <- transform(fread("Fertilizer.csv"), Fertilizer = Fertiliser)
+Fx.Method <- fread("Method.csv")
+Fx.Notes  <-  fread("Notes.csv")
+Fx.Researcher <- fread("Researcher.csv")
+Fx.Soil <- fread("Soil.csv")
+Fx.Soil.Layers <- fread("Soil.Layers.csv")
+Fx.Soil.Variables <- fread("Soil.Variables.csv")
+Fx.Weather.Station <- fread("Weather.Station.csv")
+Notes <- fread("Notes.csv")  	
+Experiment.Summary <- fread("Experiment.Summary.csv")		
+ResearcherPerExperiment <- fread("ResearchersPerExperiment.csv")	
+Experiment.Design <- data.table(read.csv("Experiment.Design.csv"), key = 'ExpID,Plot')
+Weather.Data <- data.table(read.csv("Weather.Data.csv", stringsAsFactors = FALSE))
+Weather.Data <- mutate(Weather.Data, Date = dmy(Date))
+Fertilization <- fread("Fertilization.csv")
+## traits and yields  	
+Harvest.Data <- data.table(read.csv("Harvest.Data.csv", stringsAsFactors = FALSE))
+Harvest.Data <- mutate(Harvest.Data, Date = mdy(Date), 
+              Sample = as.numeric(gsub("[Ss]", "", Harvest.Data$Sample))) 
+variables <- fread("all.variables.csv")
+save(Experiment.Summary, Fx.ClimateVar, Fx.Crop.Variables, Fx.Experiment.Field, Fx.Experiment.Site, 
+     Fx.Factors, Fx.Fertilizer, Fx.Method, Fx.Notes, Fx.Researcher, Fx.Soil, Fx.Soil.Layers, 
+     Fx.Soil.Variables, Fx.Weather.Station, Notes, ResearcherPerExperiment, Weather.Data, Harvest.Data,
+     Experiment.Design, variables, Fertilization,
+     file = "/Users/David LeBauer/R-dev/sugarbag/data/Sugarbag.RData")
 
-data(Sugarbag)
+
+#data(Sugarbag)
 
 traits <- data.table(Harvest.Data, key = c("ExpID", "Plot", "Date", "Sample"))
-traits <- melt(traits, 
+traits <- melt(traits,
                id.var = c("ExpID", "Plot", "Date", "Sample"), 
                variable.name = "variables.name", 
                value.name = "mean", na.rm = TRUE)
-traits$Date <- lubridate::dmy(traits$Date)
 
 ## cultivars
 
@@ -32,10 +67,12 @@ traits <- merge(traits, sites, by = "ExpID")
 
 
 ## Treatments
-experiment.design <- data.table(melt(Experiment.Design, id.var = c('ExpID','Treatment','Rep', 'Plot'), variable.name = "managements.type", value.var = "managements.level", na.rm = TRUE))[value != ""]
+experiment.design <- data.table(melt(Experiment.Design, id.var = c('ExpID','Treatment','Rep', 'Plot'), 
+                                     variable.name = "managements.type", value.var = "managements.level", 
+                                     na.rm = TRUE))[value != ""]
 
-### assign '1' to missing values of Replication  
-experiment.design[is.na(Rep)]$Rep <- integer(1)
+### assign '1' to missing values of Replication
+experiment.design <- mutate(experiment.design, Rep = ifelse(is.na(Rep), 1, Rep))
 
 ## Currently not clear what data we need from experiment.design:
 ## Fertilization is in Fertilization.csv
@@ -45,8 +82,7 @@ experiment.design[is.na(Rep)]$Rep <- integer(1)
 ## Variables
 
 if(sum(!unique(traits$variables.name) %in% variables$VariableName)>0){
-  stop(paste("missing", unique(traits$variables.name)[which(!unique(traits$variables.name) %in% variables$VariableName)]))
-  
+  stop(paste("missing", unique(traits$variables.name)[which(!unique(traits$variables.name) %in% variables$VariableName)]))  
 }
 
 variables <- variables[, list(variables.name = VariableName, 
@@ -74,25 +110,36 @@ harvests <- yields[, list(foo =unique(date)), by = 'ExpID']
 
 yields <- traits[variables.name %in% c("TDWT"),]
 
-
-Experiment.Design2 <- experiment.design[,-which(colnames(experiment.design) %in% c("X"))]
-
-cultivars <- Experiment.Design2[, c("ExpID", "Treatment", "Rep", "Plot", "Cultivar")]
-treatments <- cbind(Experiment.Design2[,1:4], 
-                    name = do.call(paste0, Experiment.Design2[,c(5:8,10:23)]))
+cultivars <- Experiment.Design[, list(ExpID, Treatment, Rep, Plot, Cultivar)]
+treatments <- cbind(Experiment.Design[,list(ExpID, Treatment, Rep, Plot)], 
+                    name = Experiment.Design[
+                      , paste0(CROP.CLASS, Crop.Start, Crop.Start.Date, Crop.Start.Time, Drying.Off, Drying.Off.1,
+                               FUMIGATION, Fumigation...Irrigation, Harvest, Harvest.Date, Harvest.Time, Initiation.Date, 
+                               Irrigation, Lodging.Management, N.RATE, N.Rates, N.Rates.over.2.years, Ratoon.date)])
 
 ## managements
 
-Fx.Fertilizer <- transform(Fx.Fertilizer, percentN = N./100)
-n.conversions <- Fx.Fertilizer[Fx.Fertilizer$Fertilizer %in% unique(Fertilization$Fertilizer),
-                               c("Fertilizer", "percentN")]
+Fx.Fertilizer <- transform(Fx.Fertilizer, percentN = `N%`/100)
+n.conversions <- Fx.Fertilizer[,list(Fertilizer, percentN)]
 
 Fertilization <- merge(Fertilization, n.conversions, by = "Fertilizer")
-fertilization <- with(Fertilization[Fertilization$percentN > 0,],
-                      data.frame(ExpID, Treatment, Date, fertilizerN = Amount * percentN))
-fertilization.all <- merge(treatments[,c("ExpID", "Treatment")], 
-                           fertilization[fertilization$Treatment == "ALL", -2],
-                           by = c("ExpID"))
+fertilization <- Fertilization[percentN > 0 & Amount > 0, 
+                               list(ExpID, Treatment, Date = mdy(Date), 
+                                    fertilizerN = Amount * percentN)]
+fertilization <- mutate(fertilization, year = year(Date))
+### fertilization summarized by year:
+fertilization <- fertilization[,list(fertilizerN = sum(fertilizerN), Treatment),by='ExpID,year']
+
+#### NEED TO PICK UP HERE for the merge 
+
+# melt so that 
+foo <- fertilization[Treatment == "ALL", list(year, fertilizerN), key = ExpID]
+t <- treatments[,Treatment, key = ExpID]
+merge(foo, t, all = TRUE)
+fertilization.all <- [foo]
+, 
+                           foo)
+                           by = 'ExpID')
 fertilization.notall <- fertilization[fertilization$Treatment != "ALL", ]
 fertilization <- rbind(fertilization.all, fertilization.notall)
 
@@ -142,7 +189,6 @@ ggplot(data = Y3, aes(fertilizerN, mean)) + geom_point()
 ## Reading Data Table for above ground biomass
 ABGDB <- Harvest.Data[!(is.na(Harvest.Data$DWTOTA)),c(1,3,4,5,35)]
 
-experiment.design <- Experiment.Design2
 experiment.design <- as.numeric(levels(experiment.design)[experiment.design])
 experiment.design <- data.table(as.numeric(levels(experiment.design)[experiment.design]))
 
